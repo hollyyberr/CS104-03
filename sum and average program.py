91d341247bb35db06964bf92f88bdaa3265efd3f
hollyyberr <55116533+hollyyberr@users.noreply.github.com>
2019-10-31 17:29:16 -0400

    Add files via upload

--- sum and average program.py
+++ sum and average program.py
@@ -1,14 +1,13 @@
 #Sum & Average Program
 #Holly Bernich
 #s1299693
-
-sums = []
-for i in range(0,20):
+total = 0
+listNum = []
+for x in range(0,20):
     number = int(input("Enter a number: "))
-    sums.append(number)
-print("The sum of your 20 numbers is: ", sum(sums))
-print("The average of your 20 numbers is: ", sum(sums)/len(sums))
-    
-    
-
-
+    listNum.append(number)
+ 
+for x in range(0,20):
+    total = total + listNum[x]
+print("The sum of your 20 numbers is: ", total)
+print("The average of your 20 numbers is: ", (total/len(listNum)))
